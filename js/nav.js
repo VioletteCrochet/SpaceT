f14ef99a6589e15f99ba584567b4d4a0846180cf
Crochet Olivier <olivier.crochet@ynov.com>
2018-12-05 14:30:55 +0100

    scroll

--- js/nav.js
+++ js/nav.js
@@ -1,4 +1,14 @@
+if (top >= 0 && top <= 727) {   
 
-function setActif(id) {
-    document.getElementById(id).className = "actif";
+}
+if (top >= 728 && top <= 1456) {
+    document.getElementById(navLinkOne).className = "actif";
+}
+if (top >= 1456 && top <= 2184) {
+    document.getElementById(navLinkTwo).className = "actif";
+}
+   
+}
+if (top >= 2184 && top <= 3000) {
+    document.getElementById(navLinkThree).className = "actif";
 }
