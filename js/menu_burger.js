70ab0d4687ba022d869446c5b92f18e2d0fa18e4
corentin <corentin.guillard@ynov.com>
2018-12-09 19:22:21 +0100

    modif js menu_burger

--- js/menu_burger.js
+++ js/menu_burger.js
@@ -15,11 +15,3 @@ function functionMenuBurger() {
         z.style.display = "block";
     }
 }
-
-console.log(document.getElementById("first-link"));
-
-
-document.getElementById("first-link").addEventListener('click', function () {
-    var x = document.getElementById("lp-n-links");
-    x.style.display = "none"
-})
