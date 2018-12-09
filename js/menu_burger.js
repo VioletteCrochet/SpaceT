e8ec6c06a2d1c0becc8d5ed20112917f90507293
Antonin <antonin.joulie@ynov.com>
2018-12-09 16:57:47 +0100

    Menu burger terminé

--- js/menu_burger.js
+++ js/menu_burger.js
@@ -1,9 +1,18 @@
 function functionMenuBurger() {
     var x = document.getElementById("lp-n-links");
+    var y = document.getElementById("icons_burger1");
+    var z = document.getElementById("icons_burger2");
     if (x.style.display === "block") {
         x.style.display = "none";
     } else {
         x.style.display = "block";
+    }
+    if (z.style.display === "block") {
+        y.style.display = "block";
+        z.style.display = "none";
+    } else {
+        y.style.display = "none";
+        z.style.display = "block";
     }
 }
 
