99410a17358feca2e4171ffc996037de1ff4d3aa
Crochet Olivier <olivier.crochet@ynov.com>
2018-12-09 17:58:38 +0100

    lol

--- js/nav.js
+++ js/nav.js
@@ -1,5 +1,7 @@
-if (top >= 0 && top <= 727) {   
+var top = window.scrollY;
 
+if (top >= 0 && top <= 727) {   
+                    
 }
 if (top >= 728 && top <= 1456) {
     document.getElementById(navLinkOne).className = "actif";
@@ -7,8 +9,7 @@ if (top >= 728 && top <= 1456) {
 if (top >= 1456 && top <= 2184) {
     document.getElementById(navLinkTwo).className = "actif";
 }
-   
-}
+
 if (top >= 2184 && top <= 3000) {
     document.getElementById(navLinkThree).className = "actif";
 }
