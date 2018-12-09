7a14f332cda38df1785f44eaa5e26aa917d96f42
Crochet Olivier <olivier.crochet@ynov.com>
2018-12-09 18:19:24 +0100

    oui

--- js/nav.js
+++ js/nav.js
@@ -1,14 +1,15 @@
 function linkOne() {
-
     document.getElementById("navLinkOne").className = "actif";
     document.getElementById("navLinkTwo").className -= "actif";
     document.getElementById("navLinkThree").className -= "actif";
 }
+
 function linkTwo() {
     document.getElementById("navLinkOne").className -= "actif";
     document.getElementById("navLinkTwo").className = "actif";
     document.getElementById("navLinkThree").className -= "actif";
 }
+
 function linkThree() {
     document.getElementById("navLinkOne").className -= "actif";
     document.getElementById("navLinkTwo").className -= "actif";
