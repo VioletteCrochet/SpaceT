3dbe6a6f3263213c99ccca882f83600712f218d1
Crochet Olivier <olivier.crochet@ynov.com>
2018-12-09 18:01:57 +0100

    lol2

--- js/nav.js
+++ js/nav.js
@@ -1,15 +1,17 @@
-var top = window.scrollY;
+function link(){
+    var top = window.scrollY;
 
-if (top >= 0 && top <= 727) {   
-                    
-}
-if (top >= 728 && top <= 1456) {
-    document.getElementById(navLinkOne).className = "actif";
-}
-if (top >= 1456 && top <= 2184) {
-    document.getElementById(navLinkTwo).className = "actif";
-}
+    if (top >= 0 && top <= 727) {   
+                        
+    }
+    if (top >= 728 && top <= 1456) {
+        document.getElementById(navLinkOne).className = "actif";
+    }
+    if (top >= 1456 && top <= 2184) {
+        document.getElementById(navLinkTwo).className = "actif";
+    }
 
-if (top >= 2184 && top <= 3000) {
-    document.getElementById(navLinkThree).className = "actif";
+    if (top >= 2184 && top <= 3000) {
+        document.getElementById(navLinkThree).className = "actif";
+    }
 }
